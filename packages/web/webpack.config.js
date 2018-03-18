41ab6dce696bc5c43d9995ae026d155898a39c87
secundant <mysecundant@gmail.com>
2018-03-18 03:12:51 +0300

    Base "web" package structure

--- packages/web/webpack.config.js
+++ packages/web/webpack.config.js
@@ -1,8 +1,42 @@
-/**
- * @returns {webpack.Configuration}
- */
-module.exports = () => {
+const {  } = require('webpack');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = (cliEnv, args) => {
+  const env = cliEnv || args.mode || args.env;
+
+  console.log(args.env, env);
   return {
-    
+    context: __dirname,
+    module: {
+      rules: [
+        {
+          test: /\.[tj]sx?$/,
+          use: [
+            {
+              loader: 'cache-loader'
+            },
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['env', {}]
+                ]
+              }
+            },
+            {
+              loader: 'ts-loader',
+              options: {
+                transpileOnly: true
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new ForkTsCheckerWebpackPlugin({
+        workers: ForkTsCheckerWebpackPlugin.TWO_CPUS_FREE
+      })
+    ]
   }
 };
